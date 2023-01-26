66da41cc15159743a47dbcc0d6ea8a197c1a1d57
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-01-26 17:11:26 +0900

    BooleanBuilder

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -455,6 +456,32 @@ public class QuerydslBasicTest {
         for (MemberDto memberDto : result) {
             log.info("member={}", memberDto);
         }
+    }
+
+    @Test
+    void dynamicQueryBooleanBuilder() {
+        String usernameParameter = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember1(usernameParameter, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 
 }
