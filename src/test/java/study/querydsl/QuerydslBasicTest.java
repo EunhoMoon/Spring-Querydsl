5388457f4143c61573b36c81988416ce8573a08d
eunho.moon@gmail.com <eunho.moon@gmail.com>
2023-01-26 14:59:21 +0900

    case, 상수, concat

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,9 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.extern.slf4j.Slf4j;
@@ -332,6 +335,60 @@ public class QuerydslBasicTest {
 
         for (Tuple tuple : result) {
             log.info("result={}", tuple);
+        }
+    }
+
+    @Test
+    void basicCase() {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            log.info("age={}", tuple);
+        }
+    }
+
+    @Test
+    void complexCase() {
+        List<Tuple> result = queryFactory
+                .select(member.username, new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            log.info("age={}", tuple);
+        }
+    }
+
+    @Test
+    void constant() {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            log.info("result={}", tuple);
+        }
+    }
+
+    @Test
+    void concat() {
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .fetch();
+
+        for (String str : result) {
+            log.info(str);
         }
     }
 
